c734b5c98069fd57014b8fae72b65fdabbd7d8f8
Pedro Alexis Gonzalez Espinoza <pgonzaleze@vortex.corp>
2024-09-10 17:42:00 -0600

    Hasta aqui ya trabaje con imagenes, y layouts, es importante saber como agregar hijos paginas hijas

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,7 +13,14 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Customers', 
+    href: '/dashboard/customers', 
+    icon: UserGroupIcon 
+  },
+  { name: 'Pruebas', 
+    href: '/dashboard/pruebas', 
+    icon: UserGroupIcon 
+  },
 ];
 
 export default function NavLinks() {
